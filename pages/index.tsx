bde8e9c4477fa3312fd2c42e0d569c415d9fbc2a
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-18 11:32:22 +0900

    取得してきたデータをindexページに反映

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { sampleCards } from '../utils/sample'
+import type { GetStaticProps, NextPage } from 'next'
 import Layout from '../components/Layout'
 import Card from '../components/Card'
+import { fetchPages } from '../utils/notion'
+import { IndexProps } from '../types/types'
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const { results } = await fetchPages({});
+  return {
+    props: {
+      pages: results? results : [],
+    },
+    revalidate: 10,
+  };
+}
+const Home: NextPage<IndexProps> = ({pages}) => {
   return (
     <div>
       <Layout>
         <div>
           <h1>目指せスーパーエンジニア</h1>
           <div>
-            {sampleCards.map((page, index)=>(
+            {pages.map((page, index)=>(
               <Card key={index} page={page}/>
             ))}
           </div>
