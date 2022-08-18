954e10662c38fcab31ab207fbad1a66c163a492b
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-18 11:34:28 +0900

    個別ページのデータをblock単位で反映

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,30 +1,61 @@
-import { GetServerSideProps, NextPage } from 'next'
-import React from 'react'
-import Layout from '../../components/Layout'
-import { ArticleProps, Params } from '../../types/types';
-import { sampleCards } from '../../utils/sample';
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from "next";
+import React from "react";
+import ArticleMeta from "../../components/ArticleMeta";
+import Layout from "../../components/Layout";
+import { ArticleProps, Params } from "../../types/types";
+import { fetchBlocksByPageId, fetchPages } from "../../utils/notion";
+import { getText } from "../../utils/property";
+import NotionBlocks from "notion-block-renderer";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const {slug} = context.params as Params ;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { results } = await fetchPages({});
+  const paths = results.map((page: any) => {
+    return {
+      params: {
+        slug: getText(page.properties.slug.rich_text),
+      },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+};
 
-  const page = sampleCards.find((data => data.slug === slug));
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params as Params;
 
+  const { results } = await fetchPages({ slug: slug });
+  const page = results[0];
+  const pageId = page.id;
+  const { results: blocks } = await fetchBlocksByPageId(pageId);
   return {
     props: {
       page: page,
-    }
-  }
-}
+      blocks: blocks,
+    },
+    revalidate: 10,
+  };
+};
 
-const Article: NextPage<ArticleProps> = ({page}) => {
+const Article: NextPage<ArticleProps> = ({ page, blocks }) => {
   return (
     <Layout>
       <article>
-        記事の内容
-        {page.content}
+        <div>
+          <ArticleMeta page={page} />
+        </div>
+
+        <div>
+          <NotionBlocks blocks={blocks} isCodeHighlighter={true} />
+        </div>
       </article>
     </Layout>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
