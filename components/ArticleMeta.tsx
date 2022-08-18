3923fdcd658ffa39ae9fdcc4c7254241debd4813
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-18 11:35:04 +0900

    記事のmeta情報を載せるコンポーネントを作成

---
+++ components/ArticleMeta.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { FC } from "react";
+import { ArticleMetaProps } from "../types/types";
+import { getCover, getDate, getMultiSelect, getText } from "../utils/property";
+
+const ArticleMeta: FC<ArticleMetaProps> = ({ page }) => {
+  return (
+    <>
+      {/* page cover */}
+      <Image
+        src={getCover(page.cover)}
+        alt=""
+        objectFit="cover"
+        width={640}
+        height={360}
+        quality={50}
+      />
+
+      {/* page name */}
+      <h1>{getText(page.properties.name.title)}</h1>
+      <div>
+        <div className="grid grid-cols-3 gap-4">
+          {/* published */}
+          <div className="col-span-1">Published</div>
+          <div className="col-span-2">
+            {getDate(page.properties.published.date)}
+          </div>
+
+          {/* author */}
+          <div className="col-span-1">Author</div>
+          <div className="col-span-2">
+            {getText(page.properties.author.rich_text)}
+          </div>
+
+          {/* tags */}
+          <div className="col-span-1">Tags</div>
+          <div className="col-span-2">
+            {/* change later */}
+            {getMultiSelect(page.properties.tags.multi_select).map(
+              (tag: string, index: number) => (
+                <Link key={index} href={`/tags/${tag}`}>
+                  <a>
+                    <span>{`#${tag}`}</span>
+                  </a>
+                </Link>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ArticleMeta;
