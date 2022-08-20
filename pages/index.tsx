d9ced49543cd08ed852bb0d93bb119338d5c5fea
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-20 09:59:39 +0900

    topページのレイアウトを変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,11 @@ const Home: NextPage<IndexProps> = ({pages}) => {
     <div>
       <Layout>
         <section className="text-gray-600 body-font">
-          <div className="container px-5 py-5 mx-auto">
-            <div className="flex flex-wrap -m-4">
+            <div className="grid md:gap-6 mt-10 md:grid-cols-2 lg:grid-cols-3 w-full my-12">
               {pages.map((page, index)=>(
                 <Card key={index} page={page}/>
               ))}
             </div>
-          </div>
         </section>
       </Layout>
     </div>
