f80f1bc112222d3443ff9894d954cdeb2ee0cce4
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-21 11:01:15 +0900

    Headerコンポーネントの作成

---
+++ components/templates/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Breadcrumb from '../organisms/BreadCrumb'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>目指せスーパーエンジニア</h1>
+      <Breadcrumb/>
+    </header>
+  )
+}
+
+export default Header
--- components/templates/Layout.tsx
+++ components/templates/Layout.tsx
@@ -1,14 +1,13 @@
 import React, { FC, ReactNode } from "react";
 import { LayoutProps } from "../../types/props/types";
-import Breadcrumb from "../organisms/BreadCrumb";
+import Header from "./Header";
 
 
 const Layout: FC<LayoutProps> = ({children}) => {
   return (
     <div className="relative overflow-hidden">
+      <Header/>
       <div className="flex flex-col max-w-4xl items-center w-full mx-auto">
-        <h1>目指せスーパーエンジニア</h1>
-        <Breadcrumb/>
         <main className="w-full pb-12 px-4">{children}</main>
       </div>
     </div>
