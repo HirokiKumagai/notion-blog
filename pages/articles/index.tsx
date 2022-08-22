4a0c95e1c6d6f3020a756e24d0b38549f162db51
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-23 01:07:29 +0900

    パンクズリストからarticles一覧ページに飛ぶた目のページを作成

---
+++ pages/articles/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import type { GetStaticProps, NextPage } from 'next'
+import Layout from '../../components/templates/Layout'
+import Card from '../../components/organisms/Card'
+import { fetchPages } from '../../utils/notion'
+import { IndexProps } from '../../types/props/types'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { results } = await fetchPages({});
+  return {
+    props: {
+      pages: results? results : [],
+    },
+    revalidate: 10,
+  };
+}
+const ArticleIndex: NextPage<IndexProps> = ({pages}) => {
+  return (
+    <div>
+      <Layout>
+        <section className="text-gray-600 body-font">
+            <div className="grid md:gap-6 mt-10 md:grid-cols-2 lg:grid-cols-3 w-full my-12">
+              {pages.map((page, index)=>(
+                <Card key={index} page={page}/>
+              ))}
+            </div>
+        </section>
+      </Layout>
+    </div>
+  )
+}
+
+export default ArticleIndex
