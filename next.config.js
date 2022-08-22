f2834616b6102a026ef160fd7668723a518c1022
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-22 16:33:54 +0900

    heroiconsを再インストール

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.notion.so", "images.unsplash.com"],
+    domains: ["www.notion.so", "images.unsplash.com", "tailwindui.com"],
   },
 };
 
