d41ba5e298d0c5f5ea13dd47e032296ce17c1c26
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-24 00:25:04 +0900

    reactではpropでkey属性を渡せない。呼び出し先でkeyを指定しなくても良い

--- components/atoms/TagElement.tsx
+++ components/atoms/TagElement.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
 import { TagElementProps } from "../../types/props/types";
 
-const TagElement: FC<TagElementProps> = ({tag, index}) => {
+const TagElement: FC<TagElementProps> = ({tag}) => {
   return (
     <span
-      key={index}
       className="text-sm px-2 py-1 font-normal bg-gray-200 rounded-lg break-words mr-2 mb-2"
     >
       {`#${tag}`}
