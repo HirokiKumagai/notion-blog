314f62750b880a5911b732c64acec4422b50a659
hirokikumagai <hiro.corp1212@gmail.com>
2022-08-24 13:17:26 +0900

    SSR・SSG・ISRのテスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,16 @@ import Layout from '../components/templates/Layout'
 import Card from '../components/organisms/Card'
 import { fetchPages } from '../utils/notion'
 import { IndexProps } from '../types/props/types'
+import Link from 'next/link'
 
 export const getStaticProps: GetStaticProps = async () => {
   const { results } = await fetchPages({});
   return {
     props: {
       pages: results? results : [],
-      timestamp: new Date().getTime(),
+      timestamp: new Date().toLocaleString(),
     },
-    revalidate: 15,
+    revalidate: 10,
   };
 }
 const Home: NextPage<IndexProps> = ({pages, timestamp}) => {
@@ -27,12 +28,29 @@ const Home: NextPage<IndexProps> = ({pages, timestamp}) => {
     <div>
       <Layout metaData={metaData}>
         <section className="text-gray-600 body-font">
-          <p> time: {timestamp}</p>
-            <div className="grid md:gap-6 mt-10 md:grid-cols-2 lg:grid-cols-3 w-full my-12">
-              {pages.map((page, index)=>(
-                <Card key={index} page={page}/>
-              ))}
-            </div>
+          <p>time: {timestamp}</p>
+          <div className="grid md:gap-6 mt-10 md:grid-cols-2 lg:grid-cols-3 w-full my-12">
+            {pages.map((page, index)=>(
+              <Card key={index} page={page}/>
+            ))}
+          </div>
+        <p>
+          <Link href={'/ssr-demo'}>
+            <a>SSR Demo</a>
+          </Link>
+        </p>
+
+        <p>
+          <Link href={'/ssg-demo'}>
+            <a>SSG Demo</a>
+          </Link>
+        </p>
+
+        <p>
+          <Link href={'/isr-demo'}>
+            <a>ISR Demo</a>
+          </Link>
+        </p>
         </section>
       </Layout>
     </div>
